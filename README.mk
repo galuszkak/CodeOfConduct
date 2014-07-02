848c0034c3ea2cd5cb478a9ea494c0038ed68e0b
Firemark <marpiechula@gmail.com>
2014-07-02 02:32:57 +0200

    draft

---
+++ README.mk
@@ -0,0 +1,15 @@
+- Szanujmy i nie obrażajmy się nawzajem - Hackerspace nie jest miejscem do kłótni, które zdecydowanie dzielą a nie łączą członków stowarzyszenia.
+- Ograniczmy przekleństwa - nagminne przeklinanie nie jest mile widzianie. Na spotkaniach co również na kanale IRC oraz liście dyskusyjnej.
+- Hackerspace ma być miejscem przyjaznym i dostępnym dla wszystkich, więc nękanie, wyśmiewanie czy dyskryminacja pojedynczych lub grupy członków mija się z celem całego stowarzyszenia.
+- Zakazane jest nękanie osób względem:
+	- Religii
+	- Pochodzenia (etnicznego lub narodowego)
+	- Miejscowości (Incydent z BB co wspominała Justyna?)
+	- Płci
+	- Orientacji Seksualnej
+	- Niepełnosprawności
+
+- ???
+- Nie dodawajmy zdjęć wiewiórek podczas prezentacji ;-) 
+
+Za złamanie z których powyższych punktów jest wysyłane ostrzeżenie (do 2? do 3? brak ostrzeżeń?). Jeżeli ostrzeżenia nie pomagają i dalej są łamane punkty Code of Conduct to zarząd stowarzyszenia jest zobowiązany do ocenienia sytuacji oraz ewentualnego usunięcia członka z Hackerspace oraz z kanału i listy dyskusyjnej. Osoba będaca w stowarzyszeniu i zaangażowana w konflikt nie ma prawa oceniać sytuacji oraz decyzji o wyrzuceniu danej osoby.
